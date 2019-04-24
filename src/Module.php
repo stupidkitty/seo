3f6de870f7eb75c683b0089c4439ba31e4380411
Alikdex <alikdex@gmail.com>
2019-04-24 17:30:03 +0300

    Create module

---
+++ src/Module.php
@@ -0,0 +1,56 @@
+<?php
+namespace SK\SeoModule;
+
+use Yii;
+use yii\base\Module as BaseModule;
+use yii\i18n\PhpMessageSource;
+use yii\console\Application as ConsoleApplication;
+
+/**
+ * This is the main module class of the seo extension.
+ */
+class Module extends BaseModule
+{
+    /**
+     * @var string Module cotrollers namespace.
+     */
+    public $controllerNamespace = 'SK\SeoModule\Controller';
+
+    /**
+     * @var string Default route.
+     */
+    public $defaultRoute = 'main/index';
+
+    /**
+     * @inheritdoc
+     */
+    public function __construct($id, $parent = null, $config = [])
+    {
+        // default templates path
+        $this->setViewPath(__DIR__ . '/Resources/views');
+
+        parent::__construct ($id, $parent, $config);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function init()
+    {
+        parent::init();
+
+        if (Yii::$app instanceof ConsoleApplication) {
+            $this->controllerNamespace = 'SK\SeoModule\Command';
+            $this->defaultRoute = 'run/index';
+        }
+
+        //translations
+        if (!isset(Yii::$app->get('i18n')->translations['seo'])) {
+            Yii::$app->get('i18n')->translations['seo'] = [
+                'class' => PhpMessageSource::class,
+                'basePath' => __DIR__ . '/Resources/i18n',
+                'sourceLanguage' => 'en-US',
+            ];
+        }
+    }
+}
